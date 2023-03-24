dec2512dffbec9396718cd24351a81adf5b098cb
Ricky Tharrington <Ricky.Tharrington@sas.com>
2023-03-24 16:23:33 -0400

    feat: last module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,8 +2,13 @@ import shutil
 from typing import List
 from pathlib import Path
 
-class Parser:
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssh.content import Content
 
+
+class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
@@ -13,10 +18,10 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-        with open(path,"r") as file:
+        with open(path, "r") as file:
             return file.read()
 
-    def write(self, path: Path, dest: Path, content: str, ext = ".html"):
+    def write(self, path: Path, dest: Path, content: str, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
@@ -24,9 +29,33 @@ class Parser:
     def copy(self, path: Path, source: Path, dest: Path):
         shutil.copy2(path, dest / path.relative_to(source))
 
-class ResourceParser(Parser):
 
-    extensions: List[str] = [".jpg", ".png",".gif",".css",".html"]
+class ResourceParser(Parser):
+    extensions: List[str] = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions: List[str] = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+
+
+class ReStructuredTextParser(Parser):
+    extensions: List[str] = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content, writer_name="hmtl5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
