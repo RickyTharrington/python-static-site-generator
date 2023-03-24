2537e9fe6fb64790a39d782d7235f39f495fead6
Ricky Tharrington <Ricky.Tharrington@sas.com>
2023-03-24 16:11:31 -0400

    fix: varied

--- ssg/content.py
+++ ssg/content.py
@@ -12,7 +12,7 @@ class Content(Mapping):
     def load(cls, string):
         _, fm, content = cls.__regex.split(string,2)
         metadata = load(fm,Loader=FullLoader)
-        return cls(metadata=metadata, content=content)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
@@ -20,7 +20,7 @@ class Content(Mapping):
 
     @property
     def body(self):
-        self.data["content"]
+        return self.data["content"]
 
     @property
     def type(self):
