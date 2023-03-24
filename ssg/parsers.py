bd7a9a595bb32949364806dde0fc3a8d8a8b865b
Ricky Tharrington <Ricky.Tharrington@sas.com>
2023-03-24 15:40:18 -0400

    fix: add dot to html suffix

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -26,7 +26,7 @@ class Parser:
 
 class ResourceParser(Parser):
 
-    extensions: List[str] = [".jpg", ".png",".gif",".css","html"]
+    extensions: List[str] = [".jpg", ".png",".gif",".css",".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
