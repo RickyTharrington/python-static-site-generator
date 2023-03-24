1ab264064775e33b394521672a640ce13ccef460
Ricky Tharrington <Ricky.Tharrington@sas.com>
2023-03-24 15:30:48 -0400

    feat: the Parser class

--- ssg.py
+++ ssg.py
@@ -1,11 +1,13 @@
 import typer
 
 from ssg.site import Site
+from ssg.parsers import ResourceParser
 
 def main(source="content",dest="dist"):
     config = {
         "source": source,
         "dest": dest,
+        "parsers":[ResourceParser()]
     }
 
     Site(**config).build()
